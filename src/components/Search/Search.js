504da27d98b85e0ee28b25dc6d4f0351b1e4c895
Sergey Denisov <xsergeantx@mail.ru>
2017-07-28 01:25:06 +0300

    feat: Use BooksAPI in Search component

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,18 +1,59 @@
-import React from 'react';
-import { Link } from 'react-router-dom'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import * as BooksAPI from '../../utils/BooksAPI';
+import Book from '../Book/Book';
 
-const Search = (props) => (
-  <div className="search-books">
-    <div className="search-books-bar">
-      <Link to='/' className="close-search">Close</Link>
-      <div className="search-books-input-wrapper">
-        <input type="text" placeholder="Search by title or author"/>
+class Search extends Component {
+  state = {
+    query: '',
+    books: []
+  };
+
+  search = (query) => {
+    const trimmedQuery = query.trim();
+
+    BooksAPI.search(query, this.props.maxSearchResults).then((books) => {
+      this.setState({
+        query: trimmedQuery,
+        books
+      });
+    });
+  };
+
+  render() {
+    const { query, books } = this.state;
+
+    return (
+      <div className="search-books">
+        <div className="search-books-bar">
+          <Link to='/' className="close-search">Close</Link>
+          <div className="search-books-input-wrapper">
+            <input
+              type="text"
+              placeholder="Search by title or author"
+              value={query}
+              onChange={(event) => this.search(event.target.value)}/>
+          </div>
+        </div>
+        <div className="search-books-results">
+          <ol className="books-grid">
+            {books.length > 0 && books.map((book) => (
+              <li key={book.id}>
+                <Book
+                  imageUrl={book.imageLinks.thumbnail}
+                  title={book.title}
+                  authors={book.authors && book.authors.join(', ')}/>
+              </li>
+            ))}
+          </ol>
+        </div>
       </div>
-    </div>
-    <div className="search-books-results">
-      <ol className="books-grid"/>
-    </div>
-  </div>
-);
+    )
+  };
+}
+
+Search.defaultProps = {
+  maxSearchResults: 20
+};
 
 export default Search;
