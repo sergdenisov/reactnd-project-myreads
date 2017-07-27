c119000632b21d3c025904fe95a6dac1d7b79c11
Sergey Denisov <xsergeantx@mail.ru>
2017-07-27 17:32:03 +0300

    refactor: Create and use Search component

--- src/App.js
+++ src/App.js
@@ -1,35 +1,14 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Route, Link } from 'react-router-dom'
 import * as BooksAPI from './BooksAPI'
+import Search from './components/Search/Search'
 import Book from './components/Book/Book'
 import './App.css'
 
-class BooksApp extends React.Component {
+class BooksApp extends Component {
   render() {
     return (
       <div className="app">
-        <Route path="/search" render={() => (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <Link to='/' className="close-search">Close</Link>
-              <div className="search-books-input-wrapper">
-                {/* 
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-                  
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author"/>
-                
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-          </div>
-        )}/>
         <Route exact path="/" render={() => (
           <div className="list-books">
             <div className="list-books-title">
@@ -114,6 +93,7 @@ class BooksApp extends React.Component {
             </div>
           </div>
         )}/>
+        <Route path="/search" component={Search}/>
       </div>
     )
   }
---
+++ src/components/Search/Search.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+
+const Search = (props) => (
+  <div className="search-books">
+    <div className="search-books-bar">
+      <Link to='/' className="close-search">Close</Link>
+      <div className="search-books-input-wrapper">
+        <input type="text" placeholder="Search by title or author"/>
+      </div>
+    </div>
+    <div className="search-books-results">
+      <ol className="books-grid"/>
+    </div>
+  </div>
+);
+
+export default Search;
