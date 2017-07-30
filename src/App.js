c26c366df189d5c7d99adf0433286ff2b422bf97
Sergey Denisov <xsergeantx@mail.ru>
2017-07-30 21:57:33 +0300

    feat: Load books using BooksAPI util

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react'
 import { Route } from 'react-router-dom'
+import * as BooksAPI from './utils/BooksAPI';
 import Search from './components/Search/Search'
 import BooksList from './components/BooksList/BooksList'
 import './App.css'
 
 class BooksApp extends Component {
+  state = {
+    booksByShelves: new Map()
+  };
+
+  componentDidMount() {
+    BooksAPI.getAll().then((books) => {
+      const booksByShelves = new Map();
+
+      books.forEach((book) => {
+        const booksOnShelf = booksByShelves.get(book.shelf) || [];
+        booksOnShelf.push(book);
+        booksByShelves.set(book.shelf, booksOnShelf);
+      });
+
+      this.setState({ booksByShelves });
+    });
+  }
+
   render() {
     return (
       <div className="app">
-        <Route exact path="/" component={BooksList}/>
+        <Route exact path="/" render={() => <BooksList booksByShelves={this.state.booksByShelves}/>}/>
         <Route path="/search" component={Search}/>
       </div>
     )
