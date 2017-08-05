7896a3fc403b735872e541175f5219a2d39922d3
Sergey Denisov <xsergeantx@mail.ru>
2017-08-05 20:07:25 +0300

    feat: Add spinner for main app loading state

--- src/components/BooksList/BooksList.js
+++ src/components/BooksList/BooksList.js
@@ -1,21 +1,30 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Bookshelf from '../Bookshelf/Bookshelf';
+import Spinner from '../Spinner/Spinner';
 
 const BooksList = (props) => (
   <div className="books-list">
     <div className="books-list-title">
       <h1>MyReads</h1>
     </div>
-    <div className="books-list-content">
-      {Array.from(props.booksByShelves).map(([shelf, books]) => (
+    {props.isLoading ? (
+      <div className={`books-list-content ${props.isLoading && 'books-list-content_loading'}`}>
+        <Spinner/>
+      </div>
+    ) : (
+      Array.from(props.booksByShelves).map(([shelf, books]) => (
         <Bookshelf shelf={shelf} books={books} onBookshelfChange={props.onBookshelfChange} key={shelf}/>
-      ))}
-    </div>
+      ))
+    )}
     <div className="open-search">
       <Link to='/search'>Add a book</Link>
     </div>
   </div>
 );
+
+BooksList.defaultProps = {
+  isLoading: false
+};
 
 export default BooksList;
